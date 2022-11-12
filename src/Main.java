22276620a38a6bcce40ca7bf96fb5650455f2df2
fantonv <fantonv93@gmail.com>
2022-11-12 23:28:59 +0300

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task 1
+        int salary = 15000;
+        int total = 0;
+        int i = 0;
+        while (total <= 2_459_000) {
+            i++;
+            total = total + total / 100;
+            total = total + salary;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total);
+            // в задании не указан какой процент накопелний, я поставил 12 процентов годовых.
+        }
+        //task 2
+        int u = 0;
+        while (u < 10){
+            u = u +1;
+            System.out.println(u);
+        }
+        for (int y = 10; y >= 0; y = y - 1){
+            System.out.println(y);
+        }
+
+        //task 3
+        int totalHuman = 12_000_000;
+        int totalDied = totalHuman / 1000 * 8;
+        int totalBorn = totalHuman / 1000 * 17;
+        for (int t = 1; t <= 10; t++){
+            totalHuman = totalHuman - totalDied + totalBorn;
+            System.out.println("Год " + t + " численность населения составляет " + totalHuman);
+        }
+
+        //task 4 - 6
+        int totalVas = 15000;
+        for (int r = 1; r <= 102; r++){
+                totalVas = totalVas / 100 * 7 + totalVas;
+                if (r % 6 == 0) {
+                    System.out.println("Месяц " + r + " сумма накоплений " + totalVas);
+                }
+        }
+
+        // task 7
+        int friday = 4;
+        for (; friday <= 31; friday = friday + 7){
+            System.out.println("Сегодня пятница, " + friday + " -е число. Необходимо подготовить отчет");
+        }
+
+        // task 8
+        int year = 79;
+        int ourYear = 2022;
+        int pastYears = ourYear - 200;
+        int futureYears = ourYear + 100;
+        for (; year <= futureYears; year = year + 79){
+            if (year >= pastYears){
+                System.out.println(year);
+            }
+        }
+
+        //task 9
+        int firstNbr = 2;
+        int secondNbr = 1;
+        for (;secondNbr <= 10;secondNbr++){
+            int thirdNbr = firstNbr * secondNbr;
+            System.out.println(firstNbr + "*" + secondNbr + "=" + thirdNbr);
+        }
     }
 }
